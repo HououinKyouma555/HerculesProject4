4409def0fd94913d9070355d8817e041c5191e1a
MichaelMedulla <156122669+MichaelMedulla@users.noreply.github.com>
2024-04-10 21:13:24 -0400

    Add files via upload

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -4,10 +4,8 @@ import entity.BlueBoy;
 import entity.Chicken;
 import entity.Lion;
 import entity.NPC_OldMan;
-import object.Dark_Tile;
-import object.Light_Tile;
+import entity.Shinigami;
 import object.OBJ_Door;
-import object.OBJ_Key;
 import entity.FemaleNPC;
 import entity.MaleNPC;
 import entity.FemaleNPC2;
@@ -26,40 +24,6 @@ public class AssetSetter {
         gp.obj[0] = new OBJ_Door(gp);
         gp.obj[0].worldX = gp.tileSize*10;
         gp.obj[0].worldY = gp.tileSize*6;
-
-        gp.obj[1] = new OBJ_Key(gp);
-        gp.obj[1].worldX = gp.tileSize*5;
-        gp.obj[1].worldY = gp.tileSize*5;
-
-        gp.obj[2] = new Light_Tile(gp);
-        gp.obj[2].worldX = gp.tileSize*2;
-        gp.obj[2].worldY = gp.tileSize*23;
-        gp.obj[2].appears = gp.cave;
-
-        gp.obj[3] = new Light_Tile(gp);
-        gp.obj[3].worldX = gp.tileSize*2;
-        gp.obj[3].worldY = gp.tileSize*24;
-        gp.obj[3].appears = gp.cave;
-
-        gp.obj[4] = new Light_Tile(gp);
-        gp.obj[4].worldX = gp.tileSize*2;
-        gp.obj[4].worldY = gp.tileSize*25;
-        gp.obj[4].appears = gp.cave;
-
-        gp.obj[5] = new Dark_Tile(gp);
-        gp.obj[5].worldX = gp.tileSize*47;
-        gp.obj[5].worldY = gp.tileSize*22;
-        gp.obj[5].appears = gp.Nemea;
-
-        gp.obj[6] = new Dark_Tile(gp);
-        gp.obj[6].worldX = gp.tileSize*47;
-        gp.obj[6].worldY = gp.tileSize*23;
-        gp.obj[6].appears = gp.Nemea;
-
-        gp.obj[7] = new Dark_Tile(gp);
-        gp.obj[7].worldX = gp.tileSize*47;
-        gp.obj[7].worldY = gp.tileSize*24;
-        gp.obj[7].appears = gp.Nemea;
 
     }
 
@@ -101,6 +65,8 @@ public class AssetSetter {
         gp.enemy[2] = new Lion(gp);
         gp.enemy[2].worldX = gp.tileSize*32;
         gp.enemy[2].worldY = gp.tileSize*3;
+
+        gp.enemy[3] = new Shinigami(gp);
     }
 
 }
